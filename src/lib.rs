a102282f84dc91e686aef6db4c9d8e7567aa2dd6
Wenping Guo <ybyygu@gmail.com>
2018-04-05 18:21:58 +0800

    use kick method to generate random structure

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+// [[file:~/Workspace/Programming/structure-predication/kick/kickstart.note::0bd0f9c3-bcc6-4163-a8e5-43387b9d65fe][0bd0f9c3-bcc6-4163-a8e5-43387b9d65fe]]
+extern crate cgmath;
+#[macro_use]
+extern crate timeit;
+#[macro_use]
+extern crate approx;
+
+extern crate rand;
+extern crate itertools;
+
+mod kick;
+// 0bd0f9c3-bcc6-4163-a8e5-43387b9d65fe ends here
