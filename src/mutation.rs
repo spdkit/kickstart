756bf1d9ee7953f0f2cef82407a25e290ee6e7f4
Wenping Guo <ybyygu@gmail.com>
2022-04-30 09:17:00 +0800

    Update to recent educate crate

--- src/mutation.rs
+++ src/mutation.rs
@@ -1,6 +1,4 @@
-// imports
-
-// [[file:~/Workspace/Programming/structure-predication/kickstart/kickstart.note::*imports][imports:1]]
+// [[file:../kickstart.note::*imports][imports:1]]
 use gosh::gchemol;
 
 use gchemol::prelude::*;
@@ -10,18 +8,16 @@ use spdkit::random::*;
 use crate::common::*;
 // imports:1 ends here
 
-// random bond
-
-// [[file:~/Workspace/Programming/structure-predication/kickstart/kickstart.note::*random%20bond][random bond:1]]
+// [[file:../kickstart.note::4ebcd3f9][4ebcd3f9]]
 pub fn random_bond_mutate(mol: &Molecule, degree: usize) -> Result<Molecule> {
     // import Educate trait here
     use educate::prelude::*;
 
     let mut new_mol = mol.clone();
     for _ in 0..degree {
-        new_mol = educate::tmp_random_bond_mutate(&new_mol);
+        new_mol = educate::adhoc::random_bond_mutate(&new_mol);
     }
 
     Ok(new_mol)
 }
-// random bond:1 ends here
+// 4ebcd3f9 ends here
