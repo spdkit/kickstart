7b75bf2c590cb90e92c2cb83fb7cf29a7fa2a38b
Wenping Guo <ybyygu@gmail.com>
2022-05-27 10:48:38 +0800

    New cli mod; remove redundant exports

--- src/lib.rs
+++ src/lib.rs
@@ -18,15 +18,14 @@ pub mod cli;
 // 5bd520e5 ends here
 
 // [[file:../kickstart.note::73ac23dc][73ac23dc]]
-pub use gosh::gchemol;
+use gosh::gchemol;
+use gut::prelude::*;
 
-pub use crate::config::print_default_config; // for kickstart/main
-pub use crate::core::list_db;
-pub use crate::event_loop::enter_main_loop; // for kickstart/main
-pub use crate::kickstart::kick; // for bin/kickgen
-pub use crate::search::genetic_search; // for kickstart/main
+use crate::config::print_default_config; // for kickstart/main
+use crate::core::list_db;
+use crate::search::genetic_search; // for kickstart/main
 
-pub(crate) mod common {
+mod common {
     pub use gosh::gchemol;
     pub use gut::prelude::*;
 }
@@ -35,12 +34,10 @@ pub(crate) mod common {
 // [[file:../kickstart.note::728878b3][728878b3]]
 /// Return a version string at compile time
 // mhm 0.0.28 (ac3391f 2022-05-26)
-pub fn app_version() -> String {
+fn app_version() -> String {
     let version = env!("CARGO_PKG_VERSION");
     let commit_hash = env!("GIT_HASH");
     let commit_date = env!("COMMIT_DATE");
     format!("{version} ({commit_hash} {commit_date})")
 }
-
-use gut::prelude::*;
 // 728878b3 ends here
--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,7 @@
-// [[file:../kickstart.note::*imports][imports:1]]
-use std::path::PathBuf;
-// imports:1 ends here
-
 // [[file:../kickstart.note::ae9bb309][ae9bb309]]
-use gut::cli::*;
 use gut::prelude::*;
 
-/// Chemical structure explorer
-#[derive(Debug, StructOpt)]
-#[clap(author, version, about)]
-struct Cli {
-    /// Prints default configuration.
-    #[structopt(long = "print", short = 'p')]
-    print: bool,
-
-    /// List calculated items in database.
-    #[structopt(long = "list", short = 'l')]
-    list: bool,
-
-    /// Run genetic search.
-    #[structopt(long = "run", short = 'r')]
-    run: bool,
-}
-
 fn main() -> Result<()> {
-    gut::cli::setup_logger();
-
-    let pkg_version = env!("CARGO_PKG_VERSION");
-    let pkg_name = env!("CARGO_PKG_NAME");
-
-    let args = Cli::parse();
-
-    if args.print {
-        println!("{:#^72}", " default configuration ");
-        kickstart::print_default_config();
-    } else if args.list {
-        // setup a pager like `less` cmd
-        pager::Pager::with_pager("less").setup();
-        kickstart::list_db()?;
-    } else if args.run {
-        let msg = format!(" {} v{} ", pkg_name, pkg_version);
-        println!("{:#^80}", msg);
-        kickstart::genetic_search()?;
-    } else {
-        Cli::clap().print_help()?;
-    }
-
-    Ok(())
+    kickstart::cli::enter_main()
 }
 // ae9bb309 ends here
